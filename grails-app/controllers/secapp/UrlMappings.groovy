853da3cdb635b295bc0b973685d3cae3fd4c3d2e
Ray Matthes <ray.matthes@gmail.com>
2017-05-09 16:07:09 -0600

    default page

--- grails-app/controllers/secapp/UrlMappings.groovy
+++ grails-app/controllers/secapp/UrlMappings.groovy
@@ -2,15 +2,17 @@ package secapp
 
 class UrlMappings {
 
-    static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
-            constraints {
-                // apply constraints here
-            }
-        }
+   static mappings = {
+      "/$controller/$action?/$id?(.$format)?" {
+         constraints {
+            // apply constraints here
+         }
+      }
 
-        "/"(view:"/index")
-        "500"(view:'/error')
-        "404"(view:'/notFound')
-    }
+      // "/"(view: "/index")
+      "/"(controller: 'Trade', action: 'index')
+
+      "500"(view: '/error')
+      "404"(view: '/notFound')
+   }
 }
